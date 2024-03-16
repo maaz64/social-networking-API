742b04ea447b99831e6a3f2d4031c2289063a6c4
Maaz <abumaaz728@gmail.com>
2024-03-17 01:33:11 +0530

    Minor Changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,5 +7,11 @@ router.use('/users',require('./user'));
 router.use('/post',passport.authenticate('jwt',{session:false}),require('./post'));
 router.use('/follower',passport.authenticate('jwt',{session:false}),require('./follower'));
 
+router.get('/',(_,res)=>{
+    res.status(200).json({
+        message:"Success"
+    })
+})
+
 
 module.exports = router;
