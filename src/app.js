ecc634de7638859a5dc872d5159e143f246ea406
Maaz <abumaaz728@gmail.com>
2024-03-16 23:38:46 +0530

    Added CORS

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+const app = express();
+const passportJWT = require('./config/passport-jwt');
+const errorMiddleware = require('./middleware/errorMiddleware');
+const cors = require('cors');
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit:'16kb'}));
+app.use(express.urlencoded({extended:true,limit:'1024kb'}));
+app.use(express.static("public"));
+
+app.use('/api/v1',require('./routes/index'));
+
+app.use(errorMiddleware);
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 require('dotenv').config();
-const express = require ('express');
-const app = express();
 const connectDB = require ('./config/db');
-const passportJWT = require('./config/passport-jwt');
-const errorMiddleware = require('./middleware/errorMiddleware');
+const app = require('./app');
 
 
 const Port = 8080 || process.env.Port;
 
-app.use(express.json({limit:'16kb'}));
-app.use(express.urlencoded({extended:true,limit:'1024kb'}));
-app.use(express.static("public"));
-
-app.use('/api/v1',require('./routes/index'));
-
-app.use(errorMiddleware);
 
 connectDB().then(()=>{
     app.listen(Port,(error)=>{
